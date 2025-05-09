79e64eb02efe0cd45dcef3ec90dda45a260f9855
Jakub Baran <jakbrn@users.noreply.github.com>
2025-05-09 19:19:22 +0200

    feat: start redesigning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def menu_active?(page_name)
     case page_name
     when :journal_finance
-      "active" if (
+      "btn-active" if (
         request.path_parameters[:controller].to_s == "journals" &&
         Journal.find_by_id(params[:id]) != nil &&
         Journal.find_by_id(params[:id]).journal_type_id == 1
@@ -14,7 +14,7 @@ module ApplicationHelper
       # TODO: wywalić magic number
 
     when :journal_bank
-      "active" if (
+      "btn-active" if (
         request.path_parameters[:controller].to_s == "journals" &&
         Journal.find_by_id(params[:id]) != nil &&
         Journal.find_by_id(params[:id]).journal_type_id == 2
@@ -26,61 +26,61 @@ module ApplicationHelper
       # TODO: wywalić magic number
 
     when :inventory
-      "active" if
+      "btn-active" if
         current_page?(inventory_entries_path) ||
         current_page?(new_inventory_entry_path)
 
     when :units
-      "active" if current_page?(units_path)
+      "btn-active" if current_page?(units_path)
 
     when :groups
-      "active" if current_page?(groups_path)
+      "btn-active" if current_page?(groups_path)
 
     when :categories
-      "active" if current_page?(categories_path)
+      "btn-active" if current_page?(categories_path)
 
     when :journals
-      "active" if current_page?(journals_path)
+      "btn-active" if current_page?(journals_path)
 
     when :journal_types
-      "active" if current_page?(journal_types_path)
+      "btn-active" if current_page?(journal_types_path)
 
     when :users
-      "active" if current_page?(users_path)
+      "btn-active" if current_page?(users_path)
 
     when :inventory_sources
-      "active" if current_page?(inventory_sources_path)
+      "btn-active" if current_page?(inventory_sources_path)
 
     when :audits
-      "active" if current_page?(audits_index_path)
+      "btn-active" if current_page?(audits_index_path)
 
     when :finance_report
-      "active" if current_page?(finance_report_path)
+      "btn-active" if current_page?(finance_report_path)
 
     when :finance_one_percent_report
-      "active" if current_page?(finance_one_percent_report_path)
+      "btn-active" if current_page?(finance_one_percent_report_path)
 
     when :finance_grants_report
-      "active" if current_page?(finance_grants_report_path)
+      "btn-active" if current_page?(finance_grants_report_path)
 
     when :all_finance_report
-      "active" if current_page?(all_finance_report_path)
+      "btn-active" if current_page?(all_finance_report_path)
 
     when :all_finance_one_percent_report
-      "active" if current_page?(all_finance_one_percent_report_path)
+      "btn-active" if current_page?(all_finance_one_percent_report_path)
 
     when :all_finance_grants_report
-      "active" if current_page?(all_finance_grants_report_path)
+      "btn-active" if current_page?(all_finance_grants_report_path)
 
     when :reports_dropdown
-      "active" if
+      "btn-active" if
           current_page?(finance_report_path) or
           current_page?(finance_one_percent_report_path) or
           current_page?(all_finance_report_path) or
           current_page?(all_finance_one_percent_report_path)
 
     when :administration_dropdown
-      "active" if
+      "btn-active" if
         current_page?(units_path) or
         current_page?(groups_path) or
         current_page?(categories_path) or
